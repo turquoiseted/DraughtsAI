0b02994549f7248e700854569af86902c6b3d52f
Thomas Hickman <Thomas.Hickman42@gmail.com>
2016-10-24 14:52:03 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+setup(
+    name="DraughtsAI",
+    version="1.0.0",
+    url="https://github.com/CSMaths-SEPR-Group2/DraughtsAI",
+    license="MIT",
+    description="Draughts AI",
+    platforms="any",
+    packages=['scikit-learn'],
+    install_requires=[
+        "scikit-learn>=0.0.1",
+    ]
+)
