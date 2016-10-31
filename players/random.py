19c28bbe3bdc984e5e132c1b387e72aa6c2aa182
SeaSlugUpYoButt <bleuflmo@hotmail.co.uk>
2016-10-31 14:41:12 +0000

    started with random player

---
+++ players/random.py
@@ -0,0 +1,9 @@
+from Player import Player
+
+class RandomPlayer(Player):
+    def __init__(self, board, color):
+        self._board = board
+        self._color = color
+
+    def make_move(self):
+        pass
