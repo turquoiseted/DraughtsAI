bf97f412bc6af3cec265e622797b1c0d9ac77463
Thomas Hickman <Thomas.Hickman42@gmail.com>
2016-11-01 17:22:57 +0000

    Moved Board below other classes
    This makes the compiler not complain about references

--- game_engine.py
+++ game_engine.py
@@ -1,6 +1,60 @@
 from typing import List
 from enum import Enum
 
+class Move:
+    def __init__(self, origin: Square, dest: Square, capture:Piece=None):
+        if capture is None:
+            self.captures = []  # type: List[Piece]
+        else:
+            self.captures = [capture]
+
+        self.origin = origin
+        self.steps = [dest]
+
+    def add_step(self, new_dest: Square, capture: Piece):
+        self.steps.append(new_dest)
+        self.captures.append(capture)
+
+class PieceType(Enum):
+    Black = 0
+    White = 1
+
+
+class Square:
+    def __init__(self, x, y, piece=None):
+        self.x = x
+        self.y = y
+        self.piece = piece
+
+    def set_piece(self, piece):
+        self.piece = piece
+
+    def __str__(self):
+        if self.piece is None:
+            return "."
+        else:
+            return str(self.piece)
+
+
+class Piece:
+    def __init__(self, current_square, type, is_king=False):
+        self.current_square = current_square
+        self.type = type
+        self.is_king = is_king
+
+    @property
+    def x(self):
+        return self.current_square.x
+
+    @property
+    def y(self):
+        return self.current_square.y
+
+    def __str__(self):
+        if self.type == PieceType.Black:
+            return "X"
+        else:
+            return "O"
 
 class Board:
     def __init__(self):
@@ -111,60 +165,4 @@ class Board:
             add_jump_moves(piece.x, piece.y, 1, -1, piece.type)
 
         return moves
-
-
-class Move:
-    def __init__(self, origin: Square, dest: Square, capture:Piece=None):
-        if capture is None:
-            self.captures = []  # type: List[Piece]
-        else:
-            self.captures = [capture]
-
-        self.origin = origin
-        self.steps = [dest]
-
-    def add_step(self, new_dest: Square, capture: Piece):
-        self.steps.append(new_dest)
-        self.captures.append(capture)
-
-class PieceType(Enum):
-    Black = 0
-    White = 1
-
-class Square:
-    def __init__(self, x, y, piece=None):
-        self.x = x
-        self.y = y
-        self.piece = piece
-
-    def set_piece(self, piece):
-        self.piece = piece
-
-    def __str__(self):
-        if self.piece is None:
-            return "."
-        else:
-            return str(self.piece)
-
-
-class Piece:
-    def __init__(self, current_square, type, is_king=False):
-        self.current_square = current_square
-        self.type = type
-        self.is_king = is_king
-
-    @property
-    def x(self):
-        return self.current_square.x
-
-    @property
-    def y(self):
-        return self.current_square.y
-
-    def __str__(self):
-        if self.type == PieceType.Black:
-            return "X"
-        else:
-            return "O"
-
 
