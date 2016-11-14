08d9c429edc93e44ae15b8592b51b6ad63270ac2
Thomas Hickman <Thomas.Hickman42@gmail.com>
2016-11-14 11:49:55 +0000

    Implemented draw_board and draw_piece
    Also removed set_pieces_black and set_pieces_black

--- graphics.py
+++ graphics.py
@@ -1,8 +1,8 @@
 from tkinter import *
+from typing import List
 
-from game_engine import Board, Piece, Move
+from game_engine import Board, Piece, Move, PieceType
 from helper_funcs import get_checker_pattern
-
 
 class DraughtsUI:
     def __init__(self):
@@ -17,7 +17,10 @@ class DraughtsUI:
         self._canvas.grid(column=0, row=0, sticky=(N, W, E, S))
         self._canvas.config(background="lemon chiffon")
 
-        self._ovals = []
+        self._pieces = {
+            PieceType.White: [],  # type: List[int]
+            PieceType.Black: []  # type: List[int]
+        }
 
         moveTextBox = StringVar()
         moveTextBox = Entry(self._root, width=30)
@@ -27,28 +30,46 @@ class DraughtsUI:
         moveButton = None  # TODO: add a button and receive input
 
         self._draw_main_ui()
-        self.set_pieces_black()
+        #self.set_pieces_black()
+
+    def launch(self):
         self._root.mainloop()
 
-    def draw_move(self, move: Move):
+    def apply_move(self, move: Move):
         """
-        Applies the specified move
+        Moves the ovals on the gui such that a certain move is applied
         """
-        pass
+        raise NotImplementedError()
+
+    def draw_board(self, board: Board):
+        """
+        Renders piece_by_type from a board from a blank ui state
+        """
+        for piece in board.piece_by_type[PieceType.Black] + board.piece_by_type[PieceType.White]:
+            self.draw_piece(piece)
+
+    def draw_piece(self, piece: Piece):
+        """
+        Renders a piece from a blank ui square
+        """
+        oval = self._canvas.create_oval(self._get_board_to_ui_coords(piece.x, piece.y),
+                                        fill="black" if piece.type == PieceType.Black else "white")
+
+        self._pieces[piece.type].append(oval)
 
     def _on_text_move_entered(self):
         # parse in a board class/another class, apply as a move
         pass
 
-    def _draw_board_background(self):
-        def create_rect_at_pos(row, col, fill):
-            self._canvas.create_rectangle((row * 40 + 10, col * 40 + 10, row * 40 + 50, col * 40 + 50), fill=fill)
+    def _get_board_to_ui_coords(self, row, col):
+        return row * 40 + 10, col * 40 + 10, row * 40 + 50, col * 40 + 50
 
+    def _draw_board_background(self):
         for (row, col) in get_checker_pattern(init_offset=0, init_row=0, rows=10, max_col=10):
-            create_rect_at_pos(row, col, "brown")
+            self._canvas.create_rectangle(self._get_board_to_ui_coords(row, col), fill="brown")
 
         for (row, col) in get_checker_pattern(init_offset=1, init_row=0, rows=10, max_col=10):
-            create_rect_at_pos(row, col, "orange")
+            self._canvas.create_rectangle(self._get_board_to_ui_coords(row, col), fill="orange")
 
     def _draw_main_ui(self):
         self._canvas.create_text((600, 50), text="Draughts", font=("Edwardian Script ITC", 60))
@@ -59,64 +80,13 @@ class DraughtsUI:
 
         self._draw_board_background()
 
-    def set_pieces_black(self):
-        # Row 4
-        self._canvas.create_oval((55, 375, 85, 405), fill="black")
-        self._canvas.create_oval((135, 375, 165, 405), fill="black")
-        self._canvas.create_oval((215, 375, 245, 405), fill="black")
-        self._canvas.create_oval((295, 375, 325, 405), fill="black")
-        self._canvas.create_oval((375, 375, 405, 405), fill="black")
-
-        # Row 3
-        self._canvas.create_oval((15, 335, 45, 365), fill="black")
-        self._canvas.create_oval((95, 335, 125, 365), fill="black")
-        self._canvas.create_oval((175, 335, 205, 365), fill="black")
-        self._canvas.create_oval((255, 335, 285, 365), fill="black")
-        self._canvas.create_oval((335, 335, 365, 365), fill="black")
-
-        # Row 2
-        self._canvas.create_oval((55, 295, 85, 325), fill="black")
-        self._canvas.create_oval((135, 295, 165, 325), fill="black")
-        self._canvas.create_oval((215, 295, 245, 325), fill="black")
-        self._canvas.create_oval((295, 295, 325, 325), fill="black")
-        self._canvas.create_oval((375, 295, 405, 325), fill="black")
-
-        # Row 1
-        self._canvas.create_oval((15, 255, 45, 285), fill="black")
-        self._canvas.create_oval((95, 255, 125, 285), fill="black")
-        self._canvas.create_oval((175, 255, 205, 285), fill="black")
-        self._canvas.create_oval((255, 255, 285, 285), fill="black")
-        self._canvas.create_oval((335, 255, 365, 285), fill="black")
-
-    def set_pieces_white(self):
-        # Row 1
-        self._canvas.create_oval((55, 135, 85, 165), fill="white")
-        self._canvas.create_oval((135, 135, 165, 165), fill="white")
-        self._canvas.create_oval((215, 135, 245, 165), fill="white")
-        self._canvas.create_oval((295, 135, 325, 165), fill="white")
-        self._canvas.create_oval((375, 135, 405, 165), fill="white")
-
-        # Row 2
-        self._canvas.create_oval((15, 95, 45, 125), fill="white")
-        self._canvas.create_oval((95, 95, 125, 125), fill="white")
-        self._canvas.create_oval((175, 95, 205, 125), fill="white")
-        self._canvas.create_oval((255, 95, 285, 125), fill="white")
-        self._canvas.create_oval((335, 95, 365, 125), fill="white")
-
-        # Row 3
-        self._canvas.create_oval((55, 55, 85, 85), fill="white")
-        self._canvas.create_oval((135, 55, 165, 85), fill="white")
-        self._canvas.create_oval((215, 55, 245, 85), fill="white")
-        self._canvas.create_oval((295, 55, 325, 85), fill="white")
-        self._canvas.create_oval((375, 55, 405, 85), fill="white")
-
-        # Row 4
-        self._canvas.create_oval((15, 15, 45, 45), fill="white")
-        self._canvas.create_oval((95, 15, 125, 45), fill="white")
-        self._canvas.create_oval((175, 15, 205, 45), fill="white")
-        self._canvas.create_oval((255, 15, 285, 45), fill="white")
-        self._canvas.create_oval((335, 15, 365, 45), fill="white")
-
 
 if __name__ == "__main__":
-    DraughtsUI()
+    ui = DraughtsUI()
+
+    board = Board()
+
+    board.init_default_state()
+    ui.draw_board(board)
+
+    ui.launch()
